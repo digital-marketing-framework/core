ebc5fb8557a7cf022ab5e396cb11b39b2fe7d8bc
Stephan Ude <ude@mediatis.de>
2023-07-27 18:14:53 +0200

    [TASK] config editor - tailwind embedding handling

--- config-editor-app-src/tailwind.config.js
+++ config-editor-app-src/tailwind.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // prefix: 'tw-',
+  important: true,
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
