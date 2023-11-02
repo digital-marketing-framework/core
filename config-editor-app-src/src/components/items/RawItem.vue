63df5ff0b93b1e348f2c1b3ee325a423420ea5cb
Stephan Ude <ude@mediatis.de>
2023-11-02 09:33:00 +0100

    temporarily disable line numbers in prism editor

--- config-editor-app-src/src/components/items/RawItem.vue
+++ config-editor-app-src/src/components/items/RawItem.vue
@@ -44,7 +44,7 @@ const rawIssue = computed(() => getRawIssue(props.currentPath));
                  v-model="rawValue"
                  :readonly="store.settings.readonly"
                  :highlight="highlighter"
-                 line-numbers></PrismEditor>
+                 :line-numbers="false" /> <!-- TODO enable line numbers again once the styling is fixed -->
     <div v-if="rawIssue">{{ rawIssue }}</div>
     <UuidGenerator />
 </template>
