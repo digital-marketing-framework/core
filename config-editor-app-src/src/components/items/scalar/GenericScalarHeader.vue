3e318ec3637c241956d289dfc0f066d9ad0f647e
Daniel Bagel <bagel@mediatis.de>
2024-07-16 14:15:00 +0200

    MFR-91: Style hint and description

--- config-editor-app-src/src/components/items/scalar/GenericScalarHeader.vue
+++ config-editor-app-src/src/components/items/scalar/GenericScalarHeader.vue
@@ -26,6 +26,8 @@ const props = defineProps({
 const parentSchema = computed(() => store.getSchema('..', props.currentPath, true));
 const isDynamicItem = computed(() => isDynamicChild(props.currentPath));
 const label = computed(() => getLabel(props.currentPath));
+const schema = computed(() => store.getSchema(props.currentPath, undefined, true));
+const description = computed(() => schema.value.description || 'Additional data can be provided for outbound routes, like form submissions. The data is derived from the context of the request that triggered the route, like the website language or the timestamp or request cookies or headers.');
 </script>
 
 <template>
@@ -43,4 +45,7 @@ const label = computed(() => getLabel(props.currentPath));
         <HeaderActions :currentPath="currentPath"
                        :dynamicItemPath="dynamicItemPath" />
     </header>
+    <div class="tw-pt-3 tw-text-xs tw-text-indigo-800 tw-opacity-80"
+        v-if="description">{{ description }}
+    </div>
 </template>
